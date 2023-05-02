ca3e67928e017ba17fa06a6a2b68bfd18cf787d8
MW1R1 <nickmwirigi58@gmail.com>
2023-05-02 12:52:56 +0300

    completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -3,6 +3,21 @@ import { actors } from "../data";
 
 function Actors() {
   return <div>{/*{code here}*/}</div>;
+  const displayActors= actors.map((actor)=>{
+    const moviesList = (actor.movies).map((mov)=><li key={actor.name}>{mov}</li>);
+    return (
+      <div>
+         <h1>{actor.name}</h1>
+         <ul>
+           {moviesList}
+         </ul>
+      </div>)                                 
+  })
+  return(
+  <div>
+    <h1>Actors Page</h1>
+      {displayActors}
+  </div>)
 }
 
 export default Actors;
